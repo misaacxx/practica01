581bc4225657376289da1b5914f9ce76cb43acd4
ASER <ASER@DESKTOP-22RUA1H>
2018-10-10 08:36:48 -0500

    Generando los constructores principales

---
+++ AplicacionPrueba/src/Beans/Persona.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author ASER
+ */
+public class Persona {
+   private int id_persona;
+   private String nom_persona;
+   private String ape_paterno;
+   private String ape_materno;
+
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    public String getNom_persona() {
+        return nom_persona;
+    }
+
+    public void setNom_persona(String nom_persona) {
+        this.nom_persona = nom_persona;
+    }
+
+    public String getApe_paterno() {
+        return ape_paterno;
+    }
+
+    public void setApe_paterno(String ape_paterno) {
+        this.ape_paterno = ape_paterno;
+    }
+
+    public String getApe_materno() {
+        return ape_materno;
+    }
+
+    public void setApe_materno(String ape_materno) {
+        this.ape_materno = ape_materno;
+    }
+   
+   
+}
